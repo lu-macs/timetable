c1a6eb87d0405ec27c126e7ae29417823de978f8
tonyaellie <tonya@tokia.dev>
2025-02-26 17:48:33 +0000

    fix: try fixing force reload

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
 import { registerSW } from 'virtual:pwa-register';
 
@@ -10,5 +10,5 @@ registerSW({ immediate: true });
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
