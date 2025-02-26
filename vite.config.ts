9dab23a57a7edc1014ac66ac0ac0d6058c48f05a
tonyaellie <tonya@tokia.dev>
2025-02-26 16:17:21 +0000

    feat: init

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
