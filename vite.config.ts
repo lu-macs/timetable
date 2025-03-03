2f636545be197cdbe9f2faff7bcaadbb7d2cc102
tonyaellie <tonya@tokia.dev>
2025-03-03 00:48:34 +0000

    feat: add react compiler for minor perf boost

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import path from 'path';
-import react from '@vitejs/plugin-react-swc';
+// import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', {}]],
+      },
+    }),
     VitePWA({
       registerType: 'autoUpdate',
     }),
